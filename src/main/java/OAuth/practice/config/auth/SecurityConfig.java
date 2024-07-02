9904fb87603d6b0a1a89f4a1adc2d7cc7f2fd3b0
yakkwinjiwon <yakkwinjiwon@gmail.com>
2024-07-02 23:42:47 +0900

    a

--- src/main/java/OAuth/practice/config/auth/SecurityConfig.java
+++ src/main/java/OAuth/practice/config/auth/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
